e689737eefb5dfbdd81dbedba33ed389c0acf46f
Michael Kolesnikov <dadyaaya@gmail.com>
2020-10-15 17:30:44 +0000

    added nat module

---
+++ Nat.hs
@@ -0,0 +1,21 @@
+module Nat where
+
+data Nat = Zero | Succ Nat
+  deriving (Show, Eq, Ord)
+
+instance Num Nat where
+  (+) a Zero     = a
+  (+) a (Succ b) = Succ (a + b)
+
+  (*) a Zero     = Zero
+  (*) a (Succ b) = a + (a * b)   
+
+  fromInteger 0 = Zero
+  fromInteger n = Succ (fromInteger (n-1))
+
+  abs    x    = x 
+  signum Zero = Zero
+  signum _    = Succ Zero
+
+  negate _ = error "negate is undefined for Nat"
+  
