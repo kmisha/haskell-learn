f2cffbdfce939f7e74d31e10ae9e163e8d3c4fc3
Mikhail Kolesnikov <dadyaaya@gmail.com>
2021-04-03 08:05:50 +0000

    Added functions: length, ++, null

--- Lists.hs
+++ Lists.hs
@@ -1,5 +1,19 @@
 module Lists where
 
+import Prelude hiding (length, (++), null)
+
 nTimes:: a -> Int -> [a]
 nTimes e 0 = []
 nTimes e n = e : nTimes e (n - 1)
+
+length :: [a] -> Int
+length []      = 0
+length (x: xs) = 1 + length xs
+
+(++) :: [a] -> [a] -> [a]
+(++) [] as     = as
+(++) (a: as) bs = a : as ++ bs
+
+null :: [a] -> Bool
+null [] = True
+null _  = False
