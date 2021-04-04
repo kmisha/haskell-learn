d1b0712c240d02fa86984183f15b21b57083e25e
Mikhail Kolesnikov <dadyaaya@gmail.com>
2021-04-04 07:23:11 +0000

    Added more functions to learn lists

--- Lists.hs
+++ Lists.hs
@@ -1,6 +1,7 @@
 module Lists where
 
 import Prelude hiding (length, (++), null)
+import Data.Char
 
 nTimes:: a -> Int -> [a]
 nTimes e 0 = []
@@ -31,3 +32,20 @@ isPalindrome (x: xs)
   | x == last xs = isPalindrome (init (tail xs))
   | otherwise    = False
   
+readDigits :: String -> (String, String)
+readDigits = span isDigit
+
+filterDisj :: (a -> Bool) -> (a -> Bool) -> [a] -> [a]
+filterDisj first second xs = filter (\x -> first x || second x) xs
+
+quicksort [] = []
+quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater)
+    where
+        lesser = filter (< p) xs
+        greater = filter (>= p) xs
+
+squares'n'cubes :: Num a => [a] -> [a]
+squares'n'cubes = concatMap (\x -> [x^2, x^3])
+
+delAllUpper :: String -> String
+delAllUpper str = unwords (filter (\x -> not (all isUpper x)) (words str))
